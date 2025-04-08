61a781e223d067b9d7291547119d165b05664a14
Daeun-Oh <90241178+Daeun-Oh@users.noreply.github.com>
2025-04-08 19:50:29 +0900

    과제: 뉴마커스 웹사이트 Copy (250405)

--- 02 Front-end Programming/shopping/js/common.js
+++ 02 Front-end Programming/shopping/js/common.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", function() {
-    var swiper = new Swiper(".main-banner", {
+    var swiper = new Swiper(".slide-banner", {
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
