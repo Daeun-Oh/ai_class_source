7f8a699af3b64653e96312286c94fe8aafdcc4c3
Daeun-Oh <90241178+Daeun-Oh@users.noreply.github.com>
2025-05-12 10:07:28 +0900

    250509 JSP day02

---
+++ 05 JSP-Basic/source/day02/src/main/java/org/koreait/filters/BoardFilter1.java
@@ -0,0 +1,14 @@
+package org.koreait.filters;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class BoardFilter1 implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("BoardFilter1 요청 전!");
+        chain.doFilter(request, response);
+        System.out.println("BoardFilter1 응답 후!");
+    }
+}
