898471ed47b0feab3e67eb5a37e2be55ef6b4b3a
Daeun-Oh <90241178+Daeun-Oh@users.noreply.github.com>
2025-05-12 17:48:51 +0900

    250512 JSP day02 마무리

--- 05 JSP-Basic/source/day02/src/main/java/org/koreait/filters/BoardFilter1.java
+++ 05 JSP-Basic/source/day02/src/main/java/org/koreait/filters/BoardFilter1.java
@@ -5,10 +5,20 @@ import jakarta.servlet.*;
 import java.io.IOException;
 
 public class BoardFilter1 implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String value = filterConfig.getInitParameter("fkey1");
+        System.out.println("init()");
+        System.out.println("fkey1: " + value);
+    }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("BoardFilter1 요청 전!");
+        //System.out.println("BoardFilter1 요청 전!");
+
         chain.doFilter(request, response);
-        System.out.println("BoardFilter1 응답 후!");
+
+        //System.out.println("BoardFilter1 응답 후!");
     }
 }
