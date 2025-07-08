31f8b70759554b6ac5435aeed70b94d51946d118
Daeun-Oh <90241178+Daeun-Oh@users.noreply.github.com>
2025-07-08 09:44:29 +0900

    250707 - React

---
+++ 15 React/exam07/src/App.js
@@ -0,0 +1,59 @@
+import ColorBox from './components/ColorBox';
+import { ColorProvider } from './contexts/color';
+
+const App = () => {
+  return (
+    <>
+      {/* ColorProvider: 값 교체 -> 하위에서도 가능 */}
+      <ColorProvider>
+        <ColorBox />
+      </ColorProvider>
+    </>
+  );
+};
+
+// import ColorBox from './components/ColorBox';
+// import ColorContext from './contexts/color';
+
+// const App = () => {
+//   return (
+//     <>
+//       {/* Provider: 값 교체 -> 상위에서만 가능 */}
+//       <ColorContext.Provider value={{ color: 'red' }}>
+//         <ColorBox />
+//       </ColorContext.Provider>
+//     </>
+//   );
+// };
+
+// import ColorBox from './components/ColorBox';
+// import ColorContext from './contexts/color';
+
+// const App = () => {
+//   return (
+//     <>
+//       {/* Provider: 값 교체 */}
+//       <ColorContext.Provider value={{ color: 'red' }}>
+//         <ColorBox />
+//       </ColorContext.Provider>
+//     </>
+//   );
+// };
+
+// import ColorBox from './components/ColorBox';
+// import RenderPropsEx from './components/RenderPropsEx';
+// import ColorContext from './contexts/color';
+
+// const App = () => {
+//   return (
+//     <>
+//       {/* <ColorBox /> */}
+//       {/* <RenderPropsEx>{value => (ColorBox)}</RenderPropsEx> */}
+//       <ColorContext.Consumer>
+//          <ColorBox />
+//       </ColorContext.Consumer>
+//     </>
+//   );
+// };
+
+export default App;
