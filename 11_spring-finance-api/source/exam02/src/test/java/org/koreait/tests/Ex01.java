f953f1054f8b3465479528827d2409fc6465a6df
Daeun-Oh <annsomde@naver.com>
2025-07-14 17:41:24 +0900

    250714 - Spring Data JPA

---
+++ 11_spring-finance-api/source/exam02/src/test/java/org/koreait/tests/Ex01.java
@@ -0,0 +1,45 @@
+package org.koreait.tests;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.koreait.member.entities.Member;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@Transactional
+public class Ex01 {
+
+    @PersistenceContext  // EntityManager 주입 (@Autowired 유사)
+    private EntityManager em;
+
+    @Test
+    void test1() {
+        Member member = new Member();
+        member.setSeq(1L);
+        member.setEmail("kyh@test.org");
+        member.setName("고영희");
+        member.setPassword("1234");
+        member.setCreatedAt(LocalDateTime.now());
+
+        // INSERT
+        em.persist(member);  // 영속 상태로 등록 (Entity 변화 감지 -> DB에 저장)
+
+        // UPDATE
+        member.setName("(수정)고영희");
+
+        em.detach(member);   // 영속성 분리, 변화 감지 X -> 아래 update 구문 실행 안 됨
+
+        // UPDATE
+        member.setPassword("5678");
+
+        // SELECT
+        // - 분리된 엔티티의 차이점을 비교하기 위해 SELECT 쿼리가 한 번 실행됨
+        em.merge(member);   // 분리된 영속성 상태 -> 다시 영속 상태로 변경
+
+        em.flush();
+    }
+}
---
+++ 11_spring-finance-api/source/exam02/src/test/java/org/koreait/tests/Ex02.java
@@ -0,0 +1,50 @@
+package org.koreait.tests;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.koreait.member.entities.Member;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Transactional
+@SpringBootTest
+@ActiveProfiles({"default", "test"})
+public class Ex02 {
+    @PersistenceContext
+    private EntityManager em;
+
+    @BeforeEach
+    void init() {
+        // Data 10개
+        Member member = new Member();
+        member.setSeq(1L);
+        member.setEmail("user01@test.org");
+        member.setPassword("12345678");
+        member.setName("사용자01");
+        member.setCreatedAt(LocalDateTime.now());
+
+        em.persist(member);
+        em.flush();
+        em.clear();  // 영속성 비우기
+    }
+
+    @Test
+    void test1() {
+        // find(): 기본값으로 엔티티 SELECT
+        // 영속성에 member가 없으면 SELECT 수행. 있으면 SELECT 쿼리를 수행하지 않고 바로 가져올 수 있다.
+        Member member = em.find(Member.class, 1L);
+        System.out.println(member);
+
+        // 바로 위의 코드에서 member를 영속성 상태로 올렸기 때문에, 여기선 SELECT 쿼리를 수행하지 않는다.
+        Member member2 = em.find(Member.class, 1L);
+        System.out.println(member2);
+
+        // 두 객체는 같은 객체
+        System.out.println(member == member2);
+    }
+}
